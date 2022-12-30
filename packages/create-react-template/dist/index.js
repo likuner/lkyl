8d12a5a1a8d7f7aad0d2f5b1dd66bd922bb4f59f
likun <273676636@qq.com>
2022-12-30 21:54:30 +0800

    update

--- packages/create-react-template/dist/index.js
+++ packages/create-react-template/dist/index.js
@@ -1,5 +1,5 @@
 /*!
  * @likun./create-react-template
- * version: 1.0.0-beta3
- * 2022/12/30 21:51:38
+ * version: 1.0.0-beta4
+ * 2022/12/30 21:54:16
  */(()=>{"use strict";console.log("loading1")})();
